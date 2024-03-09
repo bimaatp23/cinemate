de01f841c771b16a36cd3953cfd09314faa0192d
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-03-09 11:33:39 +0700

    Create film interface

---
+++ src/types/Film.ts
@@ -0,0 +1,8 @@
+export interface Film {
+    id: string
+    title: string
+    description: string
+    image: string
+    genre: string[]
+    imdbid: string
+}
---
+++ src/types/FilmDetail.ts
@@ -0,0 +1,11 @@
+export interface FilmDetail {
+    id: string
+    title: string
+    description: string
+    image: string
+    genre: string[]
+    rating: string
+    year: string | number
+    trailer: string
+    imdbid: string
+}
